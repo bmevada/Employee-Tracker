cf525efb757a16db3c97a72ea5008115e5f377ac
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-10-18 20:16:48 +1100

    add seeds sql file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+-- This file contains functions for performing specific SQL queries used. 
+-- The file is used to pre-populate the database, making the development of individual features much easier.
+ 
