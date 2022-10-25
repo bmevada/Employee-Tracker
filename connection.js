908e52f55d10be1cdc32e6869c30be99013f1491
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-10-25 21:25:04 +1100

    connection js file added - seperated from index file

---
+++ connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'employeeDB',
+},
+    console.log('Connected to the employeeDB database')
+);
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
